c9ded33b8039b03df1964217024e1616c1913332
Denys Krugovyy <denys.krugovyy@gmail.com>
2025-05-12 16:50:43 +0300

    Task 2

---
+++ task-2/task_2.py
@@ -0,0 +1,17 @@
+name = "Denys"
+age = 25
+hight = 1.78
+student = True
+years_to_30 = 30 - age
+
+print("Name: ", name)
+print("Age: ", age)
+print("Hight: ", hight)
+print("Student: ", student)
+
+print(type(name))
+print(type(age))
+print(type(hight))
+print(type(student))
+
+print(years_to_30)
