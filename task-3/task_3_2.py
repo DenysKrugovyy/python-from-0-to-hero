004affad3c9249966064f6062b92907e72d05672
Denys Krugovyy <denys.krugovyy@gmail.com>
2025-05-12 17:12:49 +0300

    Task 3

--- task-3/task_3_2.py
+++ task-3/task_3_2.py
@@ -2,7 +2,7 @@ temperature = int(input("Enter the temperature: "))
 
 if temperature > 30:
     print("Too hot!")
-elif temperature >= 15 and temperature <= 30:
+elif 15 <= temperature <= 30:
     print("Nice weather!")
 elif temperature < 15:
     print("Too cold!")
